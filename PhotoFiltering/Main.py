fb6e60cf44da89be05a33decfbce9d23463a7c6f
wldnjs95 <jiwonpark@utexas.edu>
2023-09-04 17:14:28 -0500

    Create Main.py

---
+++ PhotoFiltering/Main.py
@@ -0,0 +1,13 @@
+import Extractor
+import Classifier
+import Remover
+import Remover1
+import Remover2
+import Picker
+import os
+
+os.remove("extraction")
+os.remove("elimination")
+os.remove("elimination1")
+os.remove("elimination2")
+os.remove("classification")
