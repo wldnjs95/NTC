796f1c920e1967d7cb9405e39d41b653e0a9ff30
Moderator11 <65269430+Moderator11@users.noreply.github.com>
2023-09-05 18:06:24 +0900

    Picker Bug Fix (when directory does not exist)
    Picker will first check whether top directory exist at the same directory that program is running, then It will check three sub-directories of 1200, 500, 10

--- PhotoFiltering/Picker.py
+++ PhotoFiltering/Picker.py
@@ -23,6 +23,21 @@ targetPath0 = os.path.join(settings[6], '1200')
 targetPath1 = os.path.join(settings[6], '500')
 targetPath2 = os.path.join(settings[6], '10')
 
+if(os.path.isdir(settings[6]) == False):
+    print("cannot find ", settings[6], " Creating...")
+    os.mkdir(settings[6])
+
+if(os.path.isdir(targetPath0) == False):
+    print("cannot find ", targetPath0, " Creating...")
+    os.mkdir(targetPath0)
+
+if(os.path.isdir(targetPath1) == False):
+    print("cannot find ", targetPath1, " Creating...")
+    os.mkdir(targetPath1)
+
+if(os.path.isdir(targetPath2) == False):
+    print("cannot find ", targetPath2, " Creating...")
+    os.mkdir(targetPath2)
 
 for i in range(0, len(l0)):
     data = l0[i].split(',')
